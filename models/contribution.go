001b7108d602bad119686a3ea2cc828dbef0900a
Paul Schifferer <paul@schifferers.net>
2026-08-23 09:49:15 -0700

    style: gofmt

--- models/contribution.go
+++ models/contribution.go
@@ -7,7 +7,7 @@ import (
 // Contribution model.
 // This model represents the association of a person to a volume.
 type Contribution struct {
-	ID       string   `bson:"_id" json:"id" jsonapi:"primary,contribution"`
+	ID       string `bson:"_id" json:"id" jsonapi:"primary,contribution"`
 	PersonId string `bson:"person_id" json:"person_id" jsonapi:"relation,person"`
 	VolumeId string `bson:"volume_id" json:"volume_id" jsonapi:"relation,volume"`
 	Role     string `bson:"role" json:"role" jsonapi:"attr,role"`
